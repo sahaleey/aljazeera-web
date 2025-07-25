cca42f61eaf349fbc2645c7a7d9bb395d420dd2e
crazydrace <ajua46244@gmail.com>
2025-07-25 06:32:51 +0530

    add  user route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRoutes);
 app.use("/api/users", adminRoutes);
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("API is running 💨");
 });
